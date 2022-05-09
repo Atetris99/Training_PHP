c3a7409f4be2addb6e463b88b2d5c8a17796225e
yamble chris <yamble999@gmail.com>
2022-05-09 17:53:25 +0100

    Creation de l'interface utilisateur du formulaire permettant aux etudiant de renseigner leurs informations et leurs fonctions

---
+++ CRUD/ExerciceEtudiant.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Gestion des données des étudiants</title>
+</head>
+<body>
+	<form action="" method="post">
+		<p><label for="nom">Nom</label><br><input type="text" name="nom"></p>
+		<p><label for="prenom">Prenom</label><br><input type="text" name="prenom"></p>
+		<p><label for="age">Age</label><br><input type="text" name="age"></p>
+		<p><label for="Fonction">Fonction</label><br>
+			<select name="fonction" id="">
+				<option value="">Informaticien</option>
+				<option value="">Medecin</option>
+				<option value="">Mathematicien</option>
+				<option value="">Avocat</option>
+				<option value="">Juge</option>
+				<option value="">Ingenieur en bâtiment</option>
+			</select>
+		</p>
+	</form>
+</body>
+</html>
